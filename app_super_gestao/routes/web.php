a3427e06ddb723182304b1b036efadfbcf2b2bc4
Cassia <camsibr@gmail.com>
2021-08-24 19:41:10 -0300

    Menu de Navegação

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -18,14 +18,3 @@ Route::get('/',[\App\Http\Controllers\PrincipalController::class,'Principal']);
 Route::get('/contato',[\App\Http\Controllers\ContatoController::class,'Contato']);
 
 Route::get('/sobre-nos',[\App\Http\Controllers\SobreNosController::class,'SobreNos']);
-
-// nome, categoria, assunto, mensagem
-
-Route::get('/contato/{nome}/{categoria_id}',
-function(   
-    string $nome = 'Desconhecido', 
-    int $categoria_id = 1 // 1 - 'Informação'
-) {
-    echo "Estamos aqui: $nome - $categoria_id";
-}
-)->where('categoria_id','[0-9]+')->where('nome', '[A-Za-z]+');
