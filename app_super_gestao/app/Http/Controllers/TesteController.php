39e6f75034ff73fe02643435b01b26306f9793ba
Cassia <camsibr@gmail.com>
2021-08-24 21:38:44 -0300

    Encaminhando parâmetros do controller para view

--- app_super_gestao/app/Http/Controllers/TesteController.php
+++ app_super_gestao/app/Http/Controllers/TesteController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 class TesteController extends Controller
 {
     public function teste(int $sp1, int $sp2) {
-            echo "A soma de $sp1 + $sp2 é: ".($sp1+$sp2);
+            //echo "A soma de $sp1 + $sp2 é: ".($sp1+$sp2);
+            //return view('site.teste', ['x'=> $sp1, 'y'=> $sp2]); //array associativo
+            //return view('site.teste', compact('p1', 'p2')); //compact, nomes da variaveis sem o '$'
+            return view('site.teste')->with('p1', $sp1)->with('p2', $sp2); //with
     }
 }
