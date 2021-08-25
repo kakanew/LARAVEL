e5bc14fc3f25353220819cfd80875e03de2e14a0
Cassia <camsibr@gmail.com>
2021-08-24 22:14:37 -0300

    Incluíndo comentários e blocos PHP

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -22,7 +22,7 @@ Route::get('/login', function(){return 'Login';})->name('site.lo');
 
 Route::prefix('/app')->group(function() {
     Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+    Route::get('/fornecedores',[\App\Http\Controllers\FornecedorController::class,'index'])->name('app.fornecedor');
     Route::get('/produtos', function(){return 'Produtos';})->name('app.produtos');
 });
 
