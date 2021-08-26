a916c6564e187a6dd2f13d76112ac259746f408b
Cassia <camsibr@gmail.com>
2021-08-25 22:22:07 -0300

    Blade - Variável loop

--- app_super_gestao/resources/views/app/fornecedor/index.blade.php
+++ app_super_gestao/resources/views/app/fornecedor/index.blade.php
@@ -7,6 +7,11 @@
 
 @isset($fornecedores)
     @forelse($fornecedores as $indice => $fornecedor)
+        {{-- 
+        @dd ($loop)
+         --}}
+        Interação atual: {{ $loop->iteration }}
+        <br>
         Fornecedor: @{{ $fornecedor['nome'] }}
         <br>
         Status: @{{ $fornecedor['status'] }}
@@ -14,6 +19,15 @@
         CNPJ: @{{ $fornecedor['cnpj'] ?? ''}}
         <br>
         Telefone: (@{{ $fornecedor['ddd'] ?? ''}}) @{{$fornecedor['telefone'] ?? ''}}
+        <br>
+        @if($loop->first)
+            Primeira Iteração do loop
+        @endif
+        @if($loop->last)
+            Última Iteração do loop
+            <br>
+            Total de registros: {{ $loop->count }}
+        @endif
         <hr>
     @empty
         Não existem fornecedores cadastrados!!!
