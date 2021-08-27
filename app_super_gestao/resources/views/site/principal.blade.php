619dbe6f0b8ff26d6942ed097b06d4c5be0cd7f1
Cassia <camsibr@gmail.com>
2021-08-26 21:48:31 -0300

    Blade - Enviando parâmetros para componentes

--- app_super_gestao/resources/views/site/principal.blade.php
+++ app_super_gestao/resources/views/site/principal.blade.php
@@ -30,9 +30,10 @@
                     <h1>Contato</h1>
                     <p>Caso tenha qualquer dúvida por favor entre em contato com nossa equipe pelo formulário abaixo.<p>
             
-                    @component('site.layouts._components.form_contato')
+                    @component('site.layouts._components.form_contato', ['classe' => 'borda-branca'])
+                    {{-- <h3>Teste</h3> --}}
                     @endcomponent
-                    
+
                 </div>
             </div>
         </div>
